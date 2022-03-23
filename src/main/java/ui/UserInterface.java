f529b368298f5671cc7c8cff478ecfdbfcab675b
Wojciech Dąbrowski <dabrowskiw@gmail.com>
2022-03-23 09:10:09 +0100

    WIP: Added display messages form yml. Not finish yet.

--- src/main/java/ui/UserInterface.java
+++ src/main/java/ui/UserInterface.java
@@ -1,6 +1,23 @@
 package ui;
 
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Map;
+
 public class UserInterface implements Messageble{
+
+    Map<String, String> messages;
+
+    public UserInterface(){
+        try {
+            InputStream inputStream = new FileInputStream(new File("src/main/data/messages.yml"));
+            Yaml yaml = new Yaml();
+            messages = yaml.load(inputStream);
+        } catch (Exception e) {}
+    }
 
     public static void showInterface() {
         System.out.println("What do you want to do?");
@@ -13,7 +30,7 @@ public class UserInterface implements Messageble{
     }
 
     @Override
-    public String getMessage(String messageName) {
-        return null;
+    public void getMessage(String messageName) {
+        System.out.println(messages.get(messageName));
     }
 }

b0e151bb31a95b8cdd51f168a039f8cef58d191c
Wojciech Dąbrowski <dabrowskiw@gmail.com>
2022-03-25 09:53:08 +0100

    WIP: Added checkExtensionIsDocxOrXlsxOrPdf function.

--- src/main/java/logic/UserInterfaceController.java
+++ src/main/java/logic/UserInterfaceController.java
@@ -1,10 +1,9 @@
 package logic;
 
+import data.Files;
 import ui.Messageble;
 import ui.UserInterface;
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,7 +11,8 @@ public class UserInterfaceController {
 
     private static final int USER_OPTION_IS_COPY_FILE = 1;
     private static final int USER_OPTION_IS_READ_FILE = 2;
-    private Messageble messageble = new UserInterface();
+    private static final int GREATER_THAN_ZERO = 0;
+    private final Messageble messageble = new UserInterface();
 
     public int getUserDecision() {
         messageble.getMessage("welcome-message");
@@ -35,6 +35,37 @@ public class UserInterfaceController {
     }
 
     private void readPath(File file) throws Exception {
-        if (!file.exists()) throw new Exception("");
+        if (!file.exists()) throw new Exception("Path to file or directory not found");
+        if (file.isDirectory()) {
+            readDirectory(file);
+        } else {
+            readFile(file);
+        }
+    }
+
+    private void readFile(File file) {
+
+
+    }
+
+    private void readDirectory(File file) {
+
+    }
+
+    private Boolean checkExtensionIsDocxOrXlsxOrPdf(File file) {
+        String fileName = file.toString();
+        int index = fileName.lastIndexOf('.');
+        if (index > GREATER_THAN_ZERO) {
+            String fileExtension = fileName.substring(index + 1);
+            switch (fileExtension) {
+                case "docx":
+                case "xlsx":
+                case "pdf":
+                    return Boolean.TRUE;
+                default:
+                    return Boolean.FALSE;
+            }
+        }
+        return Boolean.FALSE;
     }
 }

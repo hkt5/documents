637efc60804b2fc57e5a106a00a246c3d83cb2bb
Wojciech Dąbrowski <dabrowskiw@gmail.com>
2022-04-12 22:51:27 +0200

    fix test tp ReadFile class.

--- src/test/java/logic/ReadFileTest.java
+++ src/test/java/logic/ReadFileTest.java
@@ -22,12 +22,42 @@ class ReadFileTest {
     }
 
     @Test
-    @DisplayName("Should show massage the file is pdf file.")
+    @DisplayName("Should show massage if the file is pdf file.")
     public void performWithPDFTest(@TempDir Path tempDir) throws IOException {
-        Path pdfFile = Files.createFile(tempDir.resolve("text/text.pdf"));
+        Path pdfFile = Files.createFile(tempDir.resolve("text.pdf"));
         tempFileCreator.createTempPdfFile(pdfFile);
         FileStrategy fileStrategy = new ReadFile(pdfFile.toFile());
         ResultData resultData = fileStrategy.perform();
-        assertEquals("file-is-based-on-xml", resultData.getResultMassage(),"Should return string: files-copied");
+        assertEquals("file-is-based-on-xml", resultData.getResultMassage(),"Should return string: file-is-based-on-xml");
+    }
+
+    @Test
+    @DisplayName("Should show massage if docx file is xml file.")
+    public void performWithDocxTest(@TempDir Path tempDir) throws IOException {
+        Path docxFile = Files.createFile(tempDir.resolve("text.docx"));
+        tempFileCreator.createTempDocxFile(docxFile);
+        FileStrategy fileStrategy = new ReadFile(docxFile.toFile());
+        ResultData resultData = fileStrategy.perform();
+        assertEquals("file-is-based-on-xml", resultData.getResultMassage(),"Should return string: file-is-based-on-xml");
+    }
+
+    @Test
+    @DisplayName("Should show massage if docx file is xml file.")
+    public void performWithXlsxTest(@TempDir Path tempDir) throws IOException {
+        Path xlsxFile = Files.createFile(tempDir.resolve("text.xlsx"));
+        tempFileCreator.createTempXlsXFile(xlsxFile);
+        FileStrategy fileStrategy = new ReadFile(xlsxFile.toFile());
+        ResultData resultData = fileStrategy.perform();
+        assertEquals("file-is-based-on-xml", resultData.getResultMassage(),"Should return string: file-is-based-on-xml");
+    }
+
+    @Test
+    @DisplayName("Should show massage if txt file is not xml file.")
+    public void performWithTxtTest(@TempDir Path tempDir) throws IOException {
+        Path txtFile = Files.createFile(tempDir.resolve("text.txt"));
+        tempFileCreator.createTempTxtFile(txtFile);
+        FileStrategy fileStrategy = new ReadFile(txtFile.toFile());
+        ResultData resultData = fileStrategy.perform();
+        assertEquals("file-is-based-on-xml", resultData.getResultMassage(),"Should return string: file-not-is-based-on-xml");
     }
 }

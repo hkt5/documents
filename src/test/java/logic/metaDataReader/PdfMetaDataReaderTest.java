8a74ff949c9f48f66be30b86f7393cfb62cb56fc
Wojciech Dąbrowski <dabrowskiw@gmail.com>
2022-05-07 11:49:56 +0200

    Add test MetaDataReader classes.

--- src/test/java/logic/metaDataReader/PdfMetaDataReaderTest.java
+++ src/test/java/logic/metaDataReader/PdfMetaDataReaderTest.java
@@ -1,12 +1,15 @@
 package logic.metaDataReader;
 
 import logic.TempFileCreator;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentInformation;
 import org.apache.poi.ooxml.POIXMLProperties;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -14,37 +17,37 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class DocxMetaDataReaderTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class PdfMetaDataReaderTest {
 
     private TempFileCreator tempFileCreator;
-    private DocxMetaDataReader docxMetaDataReader;
+    private PdfMetaDataReader pdfMetaDataReader;
 
     @BeforeEach
     public void setup() {
         tempFileCreator = new TempFileCreator();
-        docxMetaDataReader = new DocxMetaDataReader();
+        pdfMetaDataReader = new PdfMetaDataReader();
     }
 
     @Test
     @DisplayName("Should return 'Test keyword' keyword meta data")
-    void getMataDataFromDocxTest(@TempDir Path tempDir) throws IOException {
-        Path docxFile = Files.createFile(tempDir.resolve("test.docx"));
+    void getMataDataFromPdfTest(@TempDir Path tempDir) throws IOException {
+        Path pdfFile = Files.createFile(tempDir.resolve("test.pdf"));
         String testKeyWords = "Test keyword";
-        tempFileCreator.createTempDocxFile(docxFile);
-        FileInputStream fileInputStream = new FileInputStream(docxFile.toFile());
-        XWPFDocument xwpfDocument = new XWPFDocument(fileInputStream);
-        POIXMLProperties props = xwpfDocument.getProperties();
-        POIXMLProperties.CoreProperties coreProp=props.getCoreProperties();
-        coreProp.setKeywords(testKeyWords);
-        FileOutputStream out = new FileOutputStream(docxFile.toFile());
-        xwpfDocument.write(out);
-        out.close();
+        tempFileCreator.createTempPdfFile(pdfFile);
+        PDDocument doc = PDDocument.load(pdfFile.toFile());
+        PDDocumentInformation pdd = doc.getDocumentInformation();
+        pdd.setKeywords(testKeyWords);
+        doc.setDocumentInformation(pdd);
+        doc.save(pdfFile.toFile());
+        doc.close();
 
-        Map<String, Optional<Object>> metaDataToTest = docxMetaDataReader.getMataData(docxFile.toFile());
+        Map<String, Optional<Object>> metaDataToTest = pdfMetaDataReader.getMataData(pdfFile.toFile());
         String resultKeywords = metaDataToTest.get("Keywords").isPresent() ? (String) metaDataToTest.get("Keywords").get(): null;
         assertEquals(testKeyWords, resultKeywords);
     }
+
 
 }
--- src/test/java/logic/metaDataReader/XlsxMetaDataReaderTest.java
+++ src/test/java/logic/metaDataReader/XlsxMetaDataReaderTest.java
@@ -2,7 +2,7 @@ package logic.metaDataReader;
 
 import logic.TempFileCreator;
 import org.apache.poi.ooxml.POIXMLProperties;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,35 +14,35 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class DocxMetaDataReaderTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+class XlsxMetaDataReaderTest {
     private TempFileCreator tempFileCreator;
-    private DocxMetaDataReader docxMetaDataReader;
+    private XlsxMetaDataReader xlsxMetaDataReader;
 
     @BeforeEach
     public void setup() {
         tempFileCreator = new TempFileCreator();
-        docxMetaDataReader = new DocxMetaDataReader();
+        xlsxMetaDataReader = new XlsxMetaDataReader();
     }
 
     @Test
     @DisplayName("Should return 'Test keyword' keyword meta data")
-    void getMataDataFromDocxTest(@TempDir Path tempDir) throws IOException {
-        Path docxFile = Files.createFile(tempDir.resolve("test.docx"));
+    void getMataDataFromXlsxTest(@TempDir Path tempDir) throws IOException {
+        Path xlsxFile = Files.createFile(tempDir.resolve("test.xlsx"));
         String testKeyWords = "Test keyword";
-        tempFileCreator.createTempDocxFile(docxFile);
-        FileInputStream fileInputStream = new FileInputStream(docxFile.toFile());
-        XWPFDocument xwpfDocument = new XWPFDocument(fileInputStream);
-        POIXMLProperties props = xwpfDocument.getProperties();
-        POIXMLProperties.CoreProperties coreProp=props.getCoreProperties();
+        tempFileCreator.createTempDocxFile(xlsxFile);
+        FileInputStream fileInputStream = new FileInputStream(xlsxFile.toFile());
+        XSSFWorkbook xlsxSetMetadata = new XSSFWorkbook(fileInputStream);
+        POIXMLProperties props = xlsxSetMetadata.getProperties();
+        POIXMLProperties.CoreProperties coreProp = props.getCoreProperties();
         coreProp.setKeywords(testKeyWords);
-        FileOutputStream out = new FileOutputStream(docxFile.toFile());
-        xwpfDocument.write(out);
+        FileOutputStream out = new FileOutputStream(xlsxFile.toFile());
+        xlsxSetMetadata.write(out);
         out.close();
 
-        Map<String, Optional<Object>> metaDataToTest = docxMetaDataReader.getMataData(docxFile.toFile());
+        Map<String, Optional<Object>> metaDataToTest = xlsxMetaDataReader.getMataData(xlsxFile.toFile());
         String resultKeywords = metaDataToTest.get("Keywords").isPresent() ? (String) metaDataToTest.get("Keywords").get(): null;
         assertEquals(testKeyWords, resultKeywords);
     }
